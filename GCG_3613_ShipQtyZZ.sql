9fc8bc93c2bedf5f3cc5598ba150c58ccba20b6d
Service Web <sw@gssmail.com>
2024-03-08 14:05:12 -0600

    Commit changes in C:\Websites\Git\emarlow\3613\GCG_3613_ShipQtyZZ.sql

---
+++ GCG_3613_ShipQtyZZ.sql
@@ -0,0 +1,12 @@
+/* ==========================================
+ * TABLE: GCG_3613_ShipQtyZZ
+ * ========================================== */
+CREATE TABLE "GCG_3613_ShipQtyZZ"(
+ "Part" CHAR(17) NOT NULL ,
+ "Rev" CHAR(3),
+ "Location" CHAR(2),
+ "Qty" DOUBLE,
+ "BIRUNID" BIGINT,
+ "USAGE" NUMERIC(20,2));
+
+
