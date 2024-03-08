da7e021cafd657d65c2af622985a0ef3977f7046
Service Web <sw@gssmail.com>
2024-03-08 14:05:11 -0600

    Commit changes in C:\Websites\Git\emarlow\3613\GCG_3613_Mod_Log.sql

---
+++ GCG_3613_Mod_Log.sql
@@ -0,0 +1,14 @@
+/* ==========================================
+ * TABLE: GCG_3613_Mod_Log
+ * ========================================== */
+CREATE TABLE "GCG_3613_Mod_Log"(
+ "PART" CHAR(17),
+ "REV" CHAR(3),
+ "LOC" CHAR(2),
+ "CONTAINER" VARCHAR(15),
+ "MODIFICATION" VARCHAR(6),
+ "QUANTITY" NUMERIC(14,5),
+ "REASON_CODE" CHAR(2),
+ "MOD_DATE" DATE);
+
+
